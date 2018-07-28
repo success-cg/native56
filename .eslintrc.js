dc524b0d0f8e57c1b68d152dc26fa5094167b094
chengong <654450188@qq.com>
2018-07-28 12:39:00 +0800

    add: 添加eslint, 使用 standard  风格

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'extends': 'standard',
+  'env': {
+    'node': true,
+    'es6': true
+  },
+  'rules': {
+    'semi': ['error', 'never'],
+    'space-before-function-paren': ['error', 'never'],
+    'quotes': 'off',
+    'no-console': 'off',
+    'no-unused-vars': 'off',
+    'no-unreachable': 'off',
+    'no-redeclare': 'warn'
+  }
+}
